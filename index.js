10a587e58cae4373e34c31d6fcb4bff1d42b52cc
Maya Borrero <mlbsindia@gmail.com>
2021-10-14 16:10:23 -0400

    commit tree dc4c7c5bccf2ab593c1d3afd612ff9d87d1ca221

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your code here
+// Add your code here 
+// const body = document.getElementsByTagName("body")[0];
+// console.log(body);
+
+// function submitData(username, emailAddress) {
+
+//   let formData = {
+//     name: username,
+//     email: emailAddress
+//   };
+//   // let text = document.body.createElement("div");
+
+//   // element.append(text);
+
+//   let request = fetch('http://localhost:3000/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData)
+//   })
+//   .then (response => response.json())
+//   .then (object => console.log(object.id))
+//   .catch (function(error) {
+//     alert("Bad things! Ragnarők!")
+//     console.log(error.message)
+//   }
+// }
+
+function submitData(name, email){
+return fetch('http://localhost:3000/users',{
+  method: 'POST',
+  headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+  },
+  body: JSON.stringify({
+      name: name,
+      email: email,
+  })
+})
+.then(res => res.json())
+.then(data => renderUser(data))
+.catch(function (error){
+  console.log(error)
+  alert("Unauthorized Access")
+  document.body.append(error.message)
+})
+}
+
+function renderUser(data){
+console.log(data.id)
+document.body.append(data.id)
+}
